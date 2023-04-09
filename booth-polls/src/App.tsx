26c3acc61062f2edc79f727c3f1aa414370d2efb
Pranav9931 <64951792+Pranav9931@users.noreply.github.com>
2023-04-10 05:05:05 +0530

    Initial Commit

---
+++ booth-polls/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <Typography>Hello World!</Typography>
+    </div>
+  );
+}
+
+export default App;
